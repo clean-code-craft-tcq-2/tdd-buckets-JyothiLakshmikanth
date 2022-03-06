cb2922a93955e694ead42a1635a57a06098ba3fc
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-06 19:36:09 +0530

    Update Test-Range.h

--- Test-Range.h
+++ Test-Range.h
@@ -1,6 +1,28 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
 
+#include <vector>
+#include <map>
+#include <string>
+#define private public
 #include "Range.h"
 
+using namespace std;
 
+// void prepareExpectedMap::std::map<string, int>& expectedMap, std::set<int> input, int key)
+// {
+  
+// }
+
+TEST_CASE("OnCheckRange_CheckForOutput_FordifferentKeyValue")
+{
+  int key = 3;
+  ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  ::std::map<string, int> rangeMap, expectedMap;
+  rangeMap = checkRange(input,key);
+//   prepareExpectedMap(expectedMap, input,key);
+  expectedMap["1-3"] = 4;
+  expectedMap["4-6"] = 3;
+  expectedMap["7-9"] = 3;
+  REQUIRE(rangeMap == expectedMap);
+}
