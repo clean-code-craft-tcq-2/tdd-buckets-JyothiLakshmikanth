c8778afa0690064d7efbae701c7b2d41e1979903
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-14 19:39:01 +0530

    Update Range.cpp

--- Range.cpp
+++ Range.cpp
@@ -1,8 +1,8 @@
 #include "Range.h"
  using namespace std;
 
-Range::T_RangeMap Range::checkRange(::std::vector<int> /*input*/, int /*rangeValue*/)
+Range::T_InputList Range::checkRange(::std::vector<int> /*input*/, int /*rangeValue*/)
 {
- Range::T_RangeMap retMap;
- return retMap;
+ Range::T_InputList retList;
+ return retList;
 }
