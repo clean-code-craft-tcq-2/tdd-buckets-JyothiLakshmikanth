18f396de5e54809cac6c718ff869320c77fba57b
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-14 19:38:07 +0530

    Update Test-Range.cpp

--- Test-Range.cpp
+++ Test-Range.cpp
@@ -14,15 +14,11 @@ TEST_CASE("OnCheckRange_CheckForOutput_FordifferentKeyValue")
   int key = 3;
   ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
   ::std::vector<::std::vector<int>> actualOutput,expectedOutput;
-  //rangeMap = range.checkRange(input,key);
+  actualOutput = range.checkRange(input,key);
 
   expectedOutput.push_back({2,3,1,3});
   expectedOutput.push_back({4,6,6});
   expectedOutput.push_back({7,8,9});
-  
-  actualOutput.push_back({2,3,1,3});
-  actualOutput.push_back({4,6,6});
-  actualOutput.push_back({7,8,9});
 
   SECTION("Check for the size of maps")
   {
