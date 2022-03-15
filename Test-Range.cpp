140b6a404c6e2ddd55acbf55022be6274be648b1
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 21:07:35 +0530

    Update Test-Range.cpp

--- Test-Range.cpp
+++ Test-Range.cpp
@@ -9,19 +9,37 @@
 
 using namespace std;
 Range range;
-Range::T_RangeList prepareExpectedRangeList(::std::vector<int> input, int key)
-{
 
-  return rangelist;
-}
-TEST_CASE("OnCheckRange_CheckForBreachType_IsValidOrNot")
+TEST_CASE("ValidateTheData_CheckForBreachType_IsValidOrNot")
 {
-  int key = 3;
   ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
-  BreachType breach = range.checkRange(input,key);
-  REQUIRE(breach == VALID);
-  input = {2,4,3,6,-1,6,7,8,-9,3};
-  REQUIRE(breach == INVALID);
+  SECTION("Check For Key Value 0")
+  {
+    int key = 0;
+    BreachType breach = range.validateTheData(input,key);
+    REQUIRE(breach == INVALID);
+  }
+  SECTION("Check for negative key Value")
+  {
+    key = -1;
+    breach = range.validateTheData(input,key);
+    REQUIRE(breach == INVALID);
+  }
+  SECTION("check for Positive Key Value and then check for Sequence")
+  {
+    int key = 3;
+    SECTION("Check for Valid Sequence with Valid Key")
+    {
+       breach = range.validateTheData(input,key);
+       REQUIRE(breach == VALID);
+    }
+    SECTION("Check for Invalid Sequence with Valid Key")
+    {
+       input = {2,4,3,6,-1,6,7,8,-9,3};
+       breach = range.validateTheData(input,key);
+       REQUIRE(breach == INVALID);
+    }
+  }
 }
 
 TEST_CASE("ForTheGivenSequence_FindTheMaxAndMinValues")
@@ -36,10 +54,12 @@ TEST_CASE("ForTheGivenSequence_FindTheValidRanges")
 {
   int key = 3;
   ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  // prepare expectedRangeList
   Range::T_RangeList actualRangelist, expectedRangeList;
   expectedRangeList.push_back(make_pair(1,3));
   expectedRangeList.push_back(make_pair(4,6));
   expectedRangeList.push_back(make_pair(7,9));
+  // get actualRangelist
   actualRangelist = range.getRanges(input, key);
   SECTION("Check the size")
   {
@@ -48,14 +68,6 @@ TEST_CASE("ForTheGivenSequence_FindTheValidRanges")
     {
       REQUIRE(expectedRangeList == actualRangelist);
     }
-    SECTION("Check the content")
-    {
-      for(int i = 0; i<expectedRangeList.size();++i)
-      {
-        REQUIRE(expectedRangeList[i].first == actualRangelist[i].first);
-        REQUIRE(expectedRangeList[i].second == actualRangelist[i].second);
-      }
-    }
   }
 }
 
@@ -63,14 +75,17 @@ TEST_CASE("ForGivenSequenceRange_GetTheReadings")
 {
   int key = 3;
   ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  // prepare rangelist
   Range::T_RangeList rangelist;
   rangelist.push_back(make_pair(1,3));
   rangelist.push_back(make_pair(4,6));
   rangelist.push_back(make_pair(7,9));
   Range::T_RangeBasedReadingsList expectedRangeBasedReadingsList, actualRangeBasedReadingsList;
+  //prepare expectedRangeBasedReadingsList
   expectedRangeBasedReadingsList.push_back({2,1,3,1});
   expectedRangeBasedReadingsList.push_back({4,5,6,4});
   expectedRangeBasedReadingsList.push_back({7,9});
+  // get actualRangeBasedReadingsList
   actualRangeBasedReadingsList = range.getTheReadings(rangelist, input);
   SECTION("Check the size")
   {
@@ -79,15 +94,62 @@ TEST_CASE("ForGivenSequenceRange_GetTheReadings")
     {
       REQUIRE(expectedRangeBasedReadingsList == actualRangeBasedReadingsList);
     }
-    SECTION("Check the content")
-    {
-      for (size_t i = 0; i < actualRangeBasedReadingsList.size(); i++)
-      {
-        for(size_t j=0;j<actualRangeBasedReadingsList[i].size();j++)
-        {
-            REQUIRE(actualRangeBasedReadingsList[i][j] == expectedRangeBasedReadingsList[i][j]);
-        }
-      }
-    }
   }
+}
+
+TEST_CASE("checkTheRangeAndReadings_ForGivenSequence_checkTheOutput")
+{
+  ::string expectedOutput, actualOutput;
+  ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  SECTION("Invalid key")
+  {
+      int key = 0;
+      expectedOutput = "-";
+      actualOutput = checkTheRangeAndReadings(input, key);
+      REQUIRE(actualOutput == expectedOutput);
+  }
+  SECTION("Valid Key and Valid Sequence")
+  {
+      key = 3;
+      //prepare rangelist
+      Range::T_RangeList rangelist;
+      rangelist.push_back(make_pair(1,3));
+      rangelist.push_back(make_pair(4,6));
+      rangelist.push_back(make_pair(7,9));
+    
+    //prepare rangeBasedReadingsList
+      Range::T_RangeBasedReadingsList rangeBasedReadingsList;
+      rangeBasedReadingsList.push_back({2,1,3,1});
+      rangeBasedReadingsList.push_back({4,5,6,4});
+      rangeBasedReadingsList.push_back({7,9});
+    
+    //prepare expected output
+      char delimiter = ',';
+      char separator = '-';
+      expectedOutput.append("RANGE");
+      expectedOutput = output+delimiter;
+      expectedOutput.append("READINGS");
+      expectedOutput.append("\n");
+      for(int i = 0; i<rangelist.size();++i)
+      {
+        expectedOutput.append(std::to_string(rangelist[i].first));
+        output = output + separator;
+        output.append(std::to_string(rangelist[i].second));
+        output = output+delimiter;
+        output.append(std::to_string(rangeBasedReadingsList[i].size()));
+        output.append("\n");
+      }
+    
+    // get the actual output
+      actualOutput = checkTheRangeAndReadings(input, key);
+      REQUIRE(actualOutput == expectedOutput);
+  }
+  SECTION("Valid key and Invalid Sequence")
+  {
+    input = {2,4,3,6,1,6,7,-8,9,3};
+    expectedOutput = "-";
+        // get the actual output
+      actualOutput = checkTheRangeAndReadings(input, key);
+      REQUIRE(actualOutput == expectedOutput);
+  }   
 }
