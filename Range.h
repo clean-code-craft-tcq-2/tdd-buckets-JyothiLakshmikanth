5d3daf2abd3d7ba63292cfa58b0cdb67d7fe7f74
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 21:15:20 +0530

    Update Range.h

--- Range.h
+++ Range.h
@@ -1,15 +1,35 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 #include <map>
 
 using namespace std;
 
+typedef enum
+{
+    VALID,
+    INVALID
+}BreachType;
+
+typedef struct
+{
+    int max;
+    int min;
+}BoundValues;
+
 class Range
 {
    public:
-   typedef ::std::vector<::std::vector<int>> T_InputList;
-   T_InputList checkRange(::std::vector<int> input, int rangeValue);
+   typedef ::std::vector< ::std::vector<int>> T_RangeBasedReadingsList;
+   typedef vector<pair<size_t, size_t> > T_RangeList;
 
-   
+   ::string checkTheRangeAndReadings(::std::vector<int> input, int rangeValue);
+
+   private:
+   BoundValues getMaxAndMinValue(::vector<int> input);
+   ::string prepareOutputInCSVFormat(T_RangeList range, T_RangeBasedReadingsList readingsList);
+   T_RangeBasedReadingsList getTheReadings(T_RangeList range, ::vector<int> input);
+   T_RangeList getRanges(BoundValues values, int key);
+   BreachType validateTheData(::vector<int> input, int key);
 };
