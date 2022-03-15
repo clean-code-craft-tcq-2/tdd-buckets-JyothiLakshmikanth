834da0f6a87e2f4eb27dcbe4745f037c878a4279
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 21:29:10 +0530

    Update Range.cpp

--- Range.cpp
+++ Range.cpp
@@ -1,35 +1,35 @@
 #include "Range.h"
  using namespace std;
 
-::string Range::checkTheRangeAndReadings(::std::vector<int> input, int rangeValue)
+::string Range::checkTheRangeAndReadings(::std::vector<int> /*input*/, int /*rangeValue*/)
 {
  ::string outputCSVFormat;
  return outputCSVFormat;
 }
 
-Range::BoundValues Range::getMaxAndMinValue(::vector<int> input)
+Range::BoundValues Range::getMaxAndMinValue(::vector<int> /*input*/)
 {
  Range::BoundValues values;
  return values;
 }
 
-::string Range::prepareOutputInCSVFormat(Range::T_RangeList range, Range::T_RangeBasedReadingsList readingsList)
+::string Range::prepareOutputInCSVFormat(Range::T_RangeList /*range*/, Range::T_RangeBasedReadingsList /*readingsList*/)
 {
   ::string outputCSVFormat;
   return outputCSVFormat;
 }
 
-Range::T_RangeBasedReadingsList Range::getTheReadings(Range::T_RangeList range, ::vector<int> input)
+Range::T_RangeBasedReadingsList Range::getTheReadings(Range::T_RangeList /*range*/, ::vector<int> /*input*/)
 {
  Range::T_RangeBasedReadingsList rangeBasedReadingsList;
  return rangeBasedReadingsList;
 }
-Range::T_RangeList getRanges(BoundValues values, int key)
+Range::T_RangeList getRanges(BoundValues /*values*/, int /*key*/)
 {
  Range::T_RangeList rangList;
  return rangList;
 }
-Range::BreachType validateTheData(::vector<int> input, int key)
+Range::BreachType validateTheData(::vector<int> /*input*/, int /*key*/)
 {
  Range::BreachType breachType;
  return breachType;
