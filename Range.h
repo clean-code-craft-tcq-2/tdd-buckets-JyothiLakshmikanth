c9362054be2265e3b2b49901391ee5a4892cbc1a
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 22:48:25 +0530

    Update Range.h

--- Range.h
+++ Range.h
@@ -23,12 +23,12 @@ class Range
    typedef ::std::vector< ::std::vector<int>> T_RangeBasedReadingsList;
    typedef vector<pair<size_t, size_t> > T_RangeList;
 
-   ::string checkTheRangeAndReadings(::std::vector<int> input, int rangeValue);
+   ::string checkTheRangeAndReadings(::std::vector<int> sequence, int key);
 
    private:
-   BoundaryValues getMaxAndMinValue(::vector<int> input);
+   BoundaryValues getMaxAndMinValue(::vector<int> sequence);
    ::string prepareOutputInCSVFormat(T_RangeList range, T_RangeBasedReadingsList readingsList);
-   T_RangeBasedReadingsList getTheReadings(T_RangeList range, ::vector<int> input);
+   T_RangeBasedReadingsList getTheReadings(T_RangeList range, ::vector<int> sequence);
    T_RangeList getRanges(BoundaryValues boundaryValues, int key);
-   BreachType validateTheData(::vector<int> input, int key);
+   BreachType validateTheData(::vector<int> sequence, int key);
 };
