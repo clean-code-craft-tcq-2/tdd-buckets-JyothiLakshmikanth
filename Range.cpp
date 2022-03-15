c9252af68b531be55d8a29de56025e5da2cf5e56
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 22:49:01 +0530

    Update Range.cpp

--- Range.cpp
+++ Range.cpp
@@ -1,38 +1,108 @@
 #include "Range.h"
  using namespace std;
 
-::string Range::checkTheRangeAndReadings(::std::vector<int> /*input*/, int /*rangeValue*/)
+::string Range::checkTheRangeAndReadings(::std::vector<int> sequence, int key)
 {
- ::string outputCSVFormat;
+     ::string outputCSVFormat = "-";
+    Range::BreachType breachType = validateTheData(sequence, key);
+    if(breachType == Range::VALID)
+    {
+        breachType = Range::VALID;
+        Range::BoundaryValues values = getMaxAndMinValue(sequence);
+        Range::T_RangeList range = getRanges(values, key);
+        Range::T_RangeBasedReadingsList readingsList = getTheReadings(range, sequence);
+        outputCSVFormat = prepareOutputInCSVFormat( range, readingsList);
+    }
+
  return outputCSVFormat;
 }
 
-Range::BoundaryValues Range::getMaxAndMinValue(::vector<int> /*input*/)
+Range::BoundaryValues Range::getMaxAndMinValue(::vector<int> input)
 {
  Range::BoundaryValues boundaryValues;
+    boundaryValues.max =  *max_element(input.begin(), input.end());
+    boundaryValues.min =  *min_element(input.begin(), input.end());
  return boundaryValues;
 }
 
-::string Range::prepareOutputInCSVFormat(Range::T_RangeList /*range*/, Range::T_RangeBasedReadingsList /*readingsList*/)
+::string Range::prepareOutputInCSVFormat(Range::T_RangeList range, Range::T_RangeBasedReadingsList readingsList)
 {
   ::string outputCSVFormat;
+    char delimiter = ',';
+    char separator = '-';
+    outputCSVFormat.append("RANGE");
+    outputCSVFormat = output+delimiter;
+    outputCSVFormat.append("READINGS");
+    outputCSVFormat.append("\n");
+    for(int i = 0; i<range.size();++i)
+    {
+        outputCSVFormat.append(std::to_string(range[i].first));
+        outputCSVFormat = outputCSVFormat + separator;
+        outputCSVFormat.append(std::to_string(range[i].second));
+        outputCSVFormat = outputCSVFormat+delimiter;
+        outputCSVFormat.append(std::to_string(readingsList[i].size()));
+        outputCSVFormat.append("\n");
+    }
   return outputCSVFormat;
 }
 
-Range::T_RangeBasedReadingsList Range::getTheReadings(Range::T_RangeList /*range*/, ::vector<int> /*input*/)
+Range::T_RangeBasedReadingsList Range::getTheReadings(Range::T_RangeList range, ::vector<int> sequence)
 {
  Range::T_RangeBasedReadingsList rangeBasedReadingsList;
+    for(size_t i = 0; i < range.size(); ++i)
+    {
+        ::vector<int> readingList;
+//         cout << range[i].first << "\t" << range[i].second << endl;
+        ::vector<int>::const_iterator sequenceIt = sequence.begin();
+        for(; sequenceIt != sequence.end(); ++sequenceIt)
+        {
+            if(*sequenceIt >= range[i].first && *sequenceIt <= range[i].second)
+            {
+                readingList.push_back(*sequenceIt);
+            }
+        }
+        rangeBasedReadingsList.push_back(readingList);
+    }
+//     for (size_t i = 0; i < rangeListReadings.size(); i++)
+//     {
+//         ::cout<<"rangeListReadings["<<i<<"] size - "<<rangeListReadings[i].size()<<endl;
+//         for(size_t j=0;j<rangeListReadings[i].size();j++)
+//         {
+//             cout << "rangeListReadings["<<i<<"]"<<rangeListReadings[i][j]<< endl;
+//         }
+//     }
+
  return rangeBasedReadingsList;
 }
 
-Range::T_RangeList Range::getRanges(Range::BoundaryValues /*values*/, int /*key*/)
+Range::T_RangeList Range::getRanges(Range::BoundaryValues values, int key)
 {
  Range::T_RangeList rangList;
+    
+//     ::cout<<"min - "<<values.min<< "\t" <<"max - "<<values.max<< "\t" <<"key - "<<key<<endl;
+    for (size_t i = values.min; i < values.max; )
+    {
+        rangList.push_back(make_pair(i,i+key-1));
+        i=i+key;
+    }
+  //  ::std::cout<<"result size - "<<rangList.size()<<endl;
  return rangList;
 }
 
-Range::BreachType Range::validateTheData(::vector<int> /*input*/, int /*key*/)
+Range::BreachType Range::validateTheData(::vector<int> sequence, int key)
 {
- Range::BreachType breachType;
- return breachType;
+    Range::BreachType breachType = VALID;
+    if(key < 0)
+    {
+        breach = INVALID;
+    }
+    for(::vector<int>::const_iterator inputIt = input.begin(); inputIt!=input.end(); ++inputIt)
+    {
+        if(*inputIt<0)
+        {
+            breach = INVALID;
+            break;
+        }
+    }
+    return breach;
 }
