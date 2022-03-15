806724a0ad01f70a85333d8d46e1a86f242fb0d3
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 19:55:25 +0530

    Update Test-Range.cpp

--- Test-Range.cpp
+++ Test-Range.cpp
@@ -9,23 +9,85 @@
 
 using namespace std;
 Range range;
-TEST_CASE("OnCheckRange_CheckForOutput_FordifferentKeyValue")
+Range::T_RangeList prepareExpectedRangeList(::std::vector<int> input, int key)
+{
+
+  return rangelist;
+}
+TEST_CASE("OnCheckRange_CheckForBreachType_IsValidOrNot")
 {
   int key = 3;
   ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
-  ::std::vector<::std::vector<int>> actualOutput,expectedOutput;
-  actualOutput = range.checkRange(input,key);
+  BreachType breach = range.checkRange(input,key);
+  REQUIRE(breach == VALID);
+  input = {2,4,3,6,-1,6,7,8,-9,3};
+  REQUIRE(breach == INVALID);
+}
 
-  expectedOutput.push_back({2,3,1,3});
-  expectedOutput.push_back({4,6,6});
-  expectedOutput.push_back({7,8,9});
+TEST_CASE("ForTheGivenSequence_FindTheMaxAndMinValues")
+{
+  ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  BoundaryValues values = range.getMaxAndMinValue(input);
+  REQUIRE(values.min == 1);
+  REQUIRE(values.max == 9);
+}
 
-  SECTION("Check for the size of maps")
+TEST_CASE("ForTheGivenSequence_FindTheValidRanges")
+{
+  int key = 3;
+  ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  Range::T_RangeList actualRangelist, expectedRangeList;
+  expectedRangeList.push_back(make_pair(1,3));
+  expectedRangeList.push_back(make_pair(4,6));
+  expectedRangeList.push_back(make_pair(7,9));
+  actualRangelist = range.getRanges(input, key);
+  SECTION("Check the size")
   {
-    REQUIRE(expectedOutput.size() == actualOutput.size());
+    REQUIRE(expectedRangeList.size() == actualRangelist.size())
+    SECTION("check the list")
+    {
+      REQUIRE(expectedRangeList == actualRangelist);
+    }
+    SECTION("Check the content")
+    {
+      for(int i = 0; i<expectedRangeList.size();++i)
+      {
+        REQUIRE(expectedRangeList[i].first == actualRangelist[i].first);
+        REQUIRE(expectedRangeList[i].second == actualRangelist[i].second);
+      }
+    }
   }
-  SECTION("compare entries in rangeMap and expectedMap")
+}
+
+TEST_CASE("ForGivenSequenceRange_GetTheReadings")
+{
+  int key = 3;
+  ::std::vector<int> input = {2,4,3,6,1,6,7,8,9,3};
+  Range::T_RangeList rangelist;
+  rangelist.push_back(make_pair(1,3));
+  rangelist.push_back(make_pair(4,6));
+  rangelist.push_back(make_pair(7,9));
+  Range::T_RangeBasedReadingsList expectedRangeBasedReadingsList, actualRangeBasedReadingsList;
+  expectedRangeBasedReadingsList.push_back({2,1,3,1});
+  expectedRangeBasedReadingsList.push_back({4,5,6,4});
+  expectedRangeBasedReadingsList.push_back({7,9});
+  actualRangeBasedReadingsList = range.getTheReadings(rangelist, input);
+  SECTION("Check the size")
   {
-    REQUIRE(actualOutput == expectedOutput);
+    REQUIRE(expectedRangeBasedReadingsList.size() == actualRangeBasedReadingsList.size())
+    SECTION("check the list")
+    {
+      REQUIRE(expectedRangeBasedReadingsList == actualRangeBasedReadingsList);
+    }
+    SECTION("Check the content")
+    {
+      for (size_t i = 0; i < actualRangeBasedReadingsList.size(); i++)
+      {
+        for(size_t j=0;j<actualRangeBasedReadingsList[i].size();j++)
+        {
+            REQUIRE(actualRangeBasedReadingsList[i][j] == expectedRangeBasedReadingsList[i][j]);
+        }
+      }
+    }
   }
 }
