3e63e40d6d58a1b9ece23c9c3b4b05cd32bc325a
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-03-15 21:20:48 +0530

    Update Range.cpp

--- Range.cpp
+++ Range.cpp
@@ -1,8 +1,36 @@
 #include "Range.h"
  using namespace std;
 
-Range::T_InputList Range::checkRange(::std::vector<int> /*input*/, int /*rangeValue*/)
+::string Range::checkTheRangeAndReadings(::std::vector<int> input, int rangeValue)
 {
- Range::T_InputList retList;
- return retList;
+ ::string outputCSVFormat;
+ return outputCSVFormat;
+}
+
+Range::BoundValues Range::getMaxAndMinValue(::vector<int> input)
+{
+ Range::BoundValues values;
+ return values;
+}
+
+::string Range::prepareOutputInCSVFormat(Range::T_RangeList range, Range::T_RangeBasedReadingsList readingsList)
+{
+  ::string outputCSVFormat;
+  return outputCSVFormat;
+}
+
+Range::T_RangeBasedReadingsList Range::getTheReadings(Range::T_RangeList range, ::vector<int> input)
+{
+ Range::T_RangeBasedReadingsList rangeBasedReadingsList;
+ return rangeBasedReadingsList;
+}
+Range::T_RangeList getRanges(BoundValues values, int key)
+{
+ Range::T_RangeList rangList;
+ return rangList;
+}
+Range::BreachType validateTheData(::vector<int> input, int key)
+{
+ Range::BreachType breachType;
+ return breachType;
 }
